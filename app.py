b8a79e82acf8cc07c6f79bac321fcab54434dddb
rrrrrrockpang <ypang2@cs.washington.edu>
2024-03-03 02:06:09 -0800

    add backend to fetch data

--- app.py
+++ app.py
@@ -1,50 +1,95 @@
-class Section():
-    # a section has left and right boundaries, and top and bottom boundaries
-    def __init__(self, left, right, top, bottom):
-        self.left = left
-        self.right = right
-        self.top = top
-        self.bottom = bottom
-        
-    def is_inside(self, x, y):
-        return self.left <= x <= self.right and self.top <= y <= self.bottom
-        
+from flask import Flask, render_template, jsonify, request, session
+import duckdb
+import geopandas as gpd
+import json
 
-class Map():
-    NUM_SECTIONS = 9
+import utils
+
+app = Flask(__name__)
+app.secret_key = 'abc'
+
+# Database connection
+con = duckdb.connect(database='data/my_spatial_db.duckdb', read_only=True)
+con.execute("INSTALL 'spatial';")
+con.execute("LOAD 'spatial';")
+
+
+@app.route('/')
+def index():
+    # Serve the main page with the Mapbox GL JS map
+    return render_template('index.html')
+
+def fetch_density_data(table_name):
+    session["global_table_name"] = table_name
+    # Get bounding box parameters from the request
+    bbox = request.args.get('bbox', '')
+    if bbox:
+        bbox = [float(coord) for coord in bbox.split(',')]
+        bbox_polygon = f"POLYGON(({bbox[0]} {bbox[1]}, {bbox[2]} {bbox[1]}, {bbox[2]} {bbox[3]}, {bbox[0]} {bbox[3]}, {bbox[0]} {bbox[1]}))"
+    else:
+        # Default bounding box that covers the whole world if not specified
+        bbox_polygon = "POLYGON((-180 -90, 180 -90, 180 90, -180 90, -180 -90))"
+
+    query = f"""
+    SELECT GEOID, ppl_densit, ST_AsText(geom) AS geom_wkt
+    FROM {table_name}
+    WHERE ST_Intersects(geom, ST_GeomFromText('{bbox_polygon}'));
+    """
+
+    query_result = con.execute(query).fetchdf()
+
+    gdf = gpd.GeoDataFrame(query_result, geometry=gpd.GeoSeries.from_wkt(query_result['geom_wkt']))
+    gdf.drop(columns=['geom_wkt'], inplace=True)
     
-    def __init__(self, map_width, map_height):
-        # initialize the map that hold the boundaries of the map sections
-        self.map_width = map_width
-        self.map_height = map_height
-        self.map_sections = self.create_map_sections(map_width, map_height)
-        self.section_lookup = self.create_section_lookup()
+    geojson_data = json.loads(gdf.to_json())
+    
+    return jsonify(geojson_data)
+
+
+@app.route('/state_density_data')
+def state_density_data():
+    return fetch_density_data('state_ppl_density')
+
+@app.route('/county_density_data')
+def county_density_data():
+    return fetch_density_data('w_county_ppl_density')
+
+@app.route('/tract_density_data')
+def tract_density_data():
+    return fetch_density_data('wa_tract_ppl_density')
+
+@app.route('/stats_in_view')
+def stats_in_view():
+    minLon = request.args.get('minLon', type=float)
+    minLat = request.args.get('minLat', type=float)
+    maxLon = request.args.get('maxLon', type=float)
+    maxLat = request.args.get('maxLat', type=float)
+    
+    # print(f"minLon: {minLon}, minLat: {minLat}, maxLon: {maxLon}, maxLat: {maxLat}")
+    # logging.error(f"minLon: {minLon}, minLat: {minLat}, maxLon: {maxLon}, maxLat: {maxLat}")
+    
+    table_name = session.get('global_table_name', None)
+    stats_query = f"""
+    SELECT 
+        GEOID, ppl_densit, c_lat, c_lon
+    FROM {table_name} AS tn
+    WHERE ST_Intersects(tn.geom, ST_MakeEnvelope({minLon}, {minLat}, {maxLon}, {maxLat}));
+    """
+    
+    result = con.execute(stats_query).fetchdf()
+    
+    map = utils.Map(minLon, minLat, maxLon, maxLat)
+    polygons = []
+    for index, row in result.iterrows():
+        polygon = utils.Polygon(row['GEOID'], float(row['ppl_densit']), (float(row['c_lon']), float(row['c_lat'])))
+        polygons.append(polygon)
         
-    def create_map_sections(self, map_width, map_height):
-        # divide the map into 9 sections, split into 3 columns and 3 rows
-        map_sections = {
-            "NW": Section(0, map_width/3, 0, map_height/3),
-            "N": Section(map_width/3, 2*map_width/3, 0, map_height/3),
-            "NE": Section(2*map_width/3, map_width, 0, map_height/3),
-            "W": Section(0, map_width/3, map_height/3, 2*map_height/3),
-            "C": Section(map_width/3, 2*map_width/3, map_height/3, 2*map_height/3),
-            "E": Section(2*map_width/3, map_width, map_height/3, 2*map_height/3),
-            "SW": Section(0, map_width/3, 2*map_height/3, map_height),
-            "S": Section(map_width/3, 2*map_width/3, 2*map_height/3, map_height),
-            "SE": Section(2*map_width/3, map_width, 2*map_height/3, map_height)
-        }
-        
-        return map_sections
+    map.set_polygons(polygons)
+    map.calculate_section_densities()
+    map.rank_sections()
+    map.find_high_density_clusters()
     
-    def section_lookup(self):
-        # create a lookup table for the sections
-        section_lookup = {}
-        for section, boundaries in self.map_sections.items():
-            section_lookup[section] = boundaries
-        return section_lookup
-    
-    def get_section(self, x, y):
-        for section, boundaries in self.map_sections.items():
-            if boundaries.is_inside(x, y):
-                return section
-        return None
+    return jsonify(map.trends)
+   
+if __name__ == '__main__':
+    app.run(debug=True)
