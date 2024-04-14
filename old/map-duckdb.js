f005d59393a251d8f80f29f32ea7ae23e5c767f6
rrrrrrockpang <ypang2@cs.washington.edu>
2024-04-14 00:32:19 -0700

    aggregate old files

---
+++ old/map-duckdb.js
@@ -0,0 +1,98 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiY3Jlc2NlbmRvY2h1IiwiYSI6ImNpdGR5MWZ5aDAycjIyc3A5ZHoxZzRwMGsifQ.nEaSxm520v7TpKAy2GG_kA';
+
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/light-v10',
+    center: [-98.5795, 39.8283],
+    zoom: 4
+});
+
+function fetchAndUpdateData() {
+    var bounds = map.getBounds();
+    var zoom = map.getZoom();
+    var sourceURL = '/state_density_data'; // Default to state-level data
+
+    if (zoom >= 7) {
+        sourceURL = '/tract_density_data';
+    } else if (zoom >= 5) {
+        sourceURL = '/county_density_data';
+    }
+
+    // Construct the URL with bounds and zoom level if your API supports it
+    var apiURL = `${sourceURL}?bbox=${bounds.getWest()},${bounds.getSouth()},${bounds.getEast()},${bounds.getNorth()}&zoom=${zoom}`;
+
+    map.getSource('stateDensity').setData(apiURL);
+    updateMapAndStats();
+}
+
+function updateMapAndStats() {
+    var bounds = map.getBounds();
+    var url = `/stats_in_view?minLon=${bounds.getWest()}&minLat=${bounds.getSouth()}&maxLon=${bounds.getEast()}&maxLat=${bounds.getNorth()}`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+
+            let content = 'Hello world! <br>';
+            
+            for (const [section, trends] of Object.entries(data)) {
+                // Check if the 'high' trend exists for this section and add it to the content string
+                if (trends.high && trends.high.length > 0) {
+                    content += `<strong>${section} - High:</strong> Decision based on: ${trends.high.join(', ')}<br>`;
+                }
+        
+                // Check if the 'low' trend exists for this section and add it to the content string
+                if (trends.low && trends.low.length > 0) {
+                    content += `<strong>${section} - Low:</strong> Decision based on: ${trends.low.join(', ')}<br>`;
+                }
+            }
+            document.getElementById('stats-display').innerHTML = content;
+        })
+        .catch(error => console.error('Error fetching data:', error));
+}
+
+map.on('load', function () {
+    map.addSource('stateDensity', {
+        type: 'geojson',
+        data: {
+            "type": "FeatureCollection",
+            "features": []
+        } // Start with an empty GeoJSON FeatureCollection
+    });
+
+    map.addLayer({
+        'id': 'density-layer',
+        'type': 'fill',
+        'source': 'stateDensity',
+        'layout': {},
+        'paint': {
+            'fill-color': [
+                'interpolate',
+                ['linear'],
+                ['get', 'ppl_densit'],
+                0, '#F2F12D',    
+                36, '#EED322',   
+                72, '#E6B71E',     
+                124, '#DA9C20', 
+                188, '#CA8323',   
+                290, '#B86B25',   
+                500, '#A25626',    
+                750, '#8B4225',   
+                1000, '#723122',  
+            ],
+            'fill-opacity': 0.75
+        }
+    });
+
+    // Fetch and update data initially
+    fetchAndUpdateData();
+
+    // updateMapAndStats();
+
+    // Update data on zoom end to avoid too many requests during zooming
+    map.on('zoomend', fetchAndUpdateData);
+
+    // Update data when the map is panned
+    map.on('moveend', fetchAndUpdateData);
+});
+
